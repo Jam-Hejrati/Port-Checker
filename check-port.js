4ef52bc2e2e8fb29fa9f86eb5ea201c4340797f3
Jam <jamhejratipro@gmail.com>
2023-02-18 23:42:51 +0330

    init

---
+++ check-port.js
@@ -0,0 +1,21 @@
+import { exec } from 'child_process';
+
+// Define the range of ports to check
+const startPort = 3000;
+const endPort = 3010;
+
+// Run the netstat command to get a list of listening ports
+exec('netstat -a -n -p TCP', (err, stdout, stderr) => {
+  if (err) {
+    console.error(`Error occurred while checking ports: ${err}`);
+    return;
+  }
+  // Loop through each port in the range and check if it's in use
+  for (let port = startPort; port <= endPort; port++) {
+    if (stdout.includes(`:${port} `)) {
+      console.log(`Port ${port} is in use`);
+    } else {
+      console.log(`Port ${port} is not in use`);
+    }
+  }
+});
